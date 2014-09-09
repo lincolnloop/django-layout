3b2e4c38edda50911292eebdd2e4c56493145a7f
Collin Anderson <cmawebsite@gmail.com>
2014-09-09 08:51:58 -0400

    removed deprecated patterns()

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls.static import static
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.conf import settings
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-   # (r'', include('{{ project_name }}.apps.')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+   # url(r'', include('{{ project_name }}.apps.')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
 
 if settings.DEBUG and settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL,
