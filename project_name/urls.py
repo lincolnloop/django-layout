0f90b5420495855983d01e5fa78a6cc96b6a118b
Peter Baumgartner <pete@lincolnloop.com>
2024-02-09 16:11:59 -0700

    Use non-standard admin URL and check for 200

--- project_name/urls.py
+++ project_name/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("{{ project_name }}-admin/", admin.site.urls),
     path("-/", include("django_alive.urls")),
 ]
