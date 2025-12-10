6e364f7cbe2c39415c811eba7b7b007e4bcd45bd
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2025-12-10 13:58:27 -0500

    Add tailwindcss to project & define landing page template

--- project_name/urls.py
+++ project_name/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("{{ project_name }}-admin/", admin.site.urls),
     path("-/", include("django_alive.urls")),
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="index.html",
+            extra_context={"project_name": "{{ project_name }}"},
+        ),
+        name="index",
+    ),
 ]
